f24fce7c56deebbc5b5adb175b740a8f18c5ca99
Qingbo <qingbo.zhang@rea-group.com>
2016-02-22 19:17:59 +0800

    make wifi config global

--- init.lua
+++ init.lua
@@ -1,12 +1,4 @@
-light_pin=2
-sensor_pin = 3
-relay_pin=4
-
-gpio.mode(sensor_pin,  gpio.INPUT)
-gpio.mode(light_pin,  gpio.OUTPUT)
-gpio.mode(relay_pin,  gpio.OUTPUT)
-
-l = file.list();
+l = file.list()
 for k,v in pairs(l) do
     print("name:"..k..", size:"..v)
 end
@@ -15,25 +7,23 @@ function run()
     dofile("mqtt.lua")
 end
 
+ssid, password, bssid_set, bssid = wifi.sta.getconfig()
 function print_config()
-    ssid, password, bssid_set, bssid=wifi.sta.getconfig()
     print("\nCurrent Station configuration:\nSSID : "..ssid
     .."\nPassword : "..password
     .."\nBSSID_set : "..bssid_set
     .."\nBSSID: "..bssid.."\n")
-    return ssid
 end
 wifi.setmode(wifi.STATION)
-if(print_config() ~= '') then
+if(ssid ~= '') then
     wifi.sta.autoconnect(1)
-
     tmr.alarm(1,2000, 1, function() 
       if wifi.sta.getip()==nil then 
          print(" Wait for IP --> "..wifi.sta.status()) 
       else 
          print("New IP address is "..wifi.sta.getip()) 
          tmr.stop(1) 
-        print('load mqtt')
+         print('load mqtt')
             run()
         end 
     end)
@@ -43,5 +33,4 @@ else
      print_config()
      run()
     end)
-
 end
