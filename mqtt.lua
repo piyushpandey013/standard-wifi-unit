bba3f03ee71aac927bc066c81f18413413106f4e
Qingbo <qingbo.zhang@rea-group.com>
2016-02-22 21:23:06 +0800

    move esp8266 code to server

--- mqtt.lua
+++ mqtt.lua
@@ -1,64 +1,42 @@
 HOST = "192.168.199.8"
 PORT = "1883"
-TARGET_TEMP=26
 
 client = mqtt.Client(node.chipid(), 30000)
 
-function blink(n)
-    for i=n, 1, -1
-    do 
-      gpio.write(light_pin,gpio.HIGH);
-      tmr.delay(500000)
-      gpio.write(light_pin,gpio.LOW);
-    end 
-end
+attr_config = {}
 chip_id = node.chipid()
-t = require("ds18b20")
-function setup_sensor(t) 
-    -- ESP-01 GPIO Mapping
-    t.setup(sensor_pin)
-    addrs = t.addrs()
-    if (addrs ~= nil) then
-      print("Total DS18B20 sensors: "..table.getn(addrs))
-    end
-    -- Just read temperature
-    print("Temperature: "..t.read().."'C")
-end
-setup_sensor(t)
-
--- on receive message
-function onMessage(conn, topic, data)
-  if data ~= nil and topic == 'heater' then
-    print(topic..":"..data)
-    if data=='Off' then
-        gpio.write(light_pin,gpio.LOW);
-        gpio.write(relay_pin,gpio.HIGH);
-    else 
-        gpio.write(light_pin,gpio.HIGH);
-        gpio.write(relay_pin,gpio.LOW);
-    end
-  end
-end
-
-function broadcast_temp()
-    response = {}
-    response["id"] = chip_id 
-    tmr.alarm(0, 5000, 1, function()
-        response["temperature"] = t.read()
-        client:publish("temperature",cjson.encode(response),0,0, function(conn) end)
-    end)
-end
 
 function onConnect()
   print('connected')
-  client:subscribe( "/all", 0, function() print("subscribed") end)
-  client:subscribe( "/announcements", 0, function() print("subscribed") end)
-  client:subscribe( "/chip/" .. chip_id, 0, function() print("subscribed") end)
-  client:subscribe( "/ip/" .. wifi.sta.getip(), 0, function() print("subscribed") end)
-  client:subscribe("heater",0, function() print("subscribe success") end)
-  broadcast_temp()
+  register_info = {}
+  register_info["id"] = chip_id
+  register_info["ip"] = wifi.sta.getip()
+  register_info["mac"] = wifi.ap.getmac()
+  register_info["ssid"] = ssid
+  client:publish("/register",cjson.encode(register_info),0,0, function(conn) end)
+  client:subscribe( "/config/" .. chip_id, 0, function(conn) print("/config/") end)
+  client:subscribe( "/run/" .. chip_id, 0, function(conn) print("/run/") end)
+  client:subscribe( "/command/" .. chip_id, 0, function(conn) print("/command/") end)
 end
 
+function eval_string(string)
+  local func = loadstring(string)
+  if(func) then
+    func()
+  end
+end            
+
+function onMessage(conn, topic, data)
+    print(data)
+   if topic=="/config/" .. chip_id then
+      eval_string(data)
+   elseif topic=="/run/" .. chip_id then
+       eval_string(data)
+   elseif topic=="/command/" .. chip_id then
+       eval_string(data)
+   else
+       client:publish("/noice/" .. chip_id ,"##" .. topic .. "##" .. data ,0,0, function(conn) end)
+   end        
+end
 client:connect(HOST, PORT, 0, onConnect)
 client:on("message", onMessage)
-
