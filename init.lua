6c5086fd0e7990cf5d0c54b1822436385335ce66
Qingbo <qingbo.zhang@rea-group.com>
2016-05-18 22:48:56 +0800

    use enduser setup module instead of smart config

--- init.lua
+++ init.lua
@@ -9,42 +9,58 @@ gpio.write(light_pin, gpio.LOW)
 function run()
     dofile("mqtt.lua")
 end
-wifi.setmode(wifi.STATION)
-function print_config()
-  ssid, password , bssid_set, bssid = wifi.sta.getconfig()
-  print("\nCurrent Station configuration:\nSSID : "..ssid
-          .."\nPassword : "..password
-          .."\nBSSID_set : "..bssid_set
-          .."\nBSSID: "..bssid.."\n")
-end
+--wifi.setmode(wifi.STATION)
+--function print_config()
+--  ssid, password , bssid_set, bssid = wifi.sta.getconfig()
+--  print("\nCurrent Station configuration:\nSSID : "..ssid
+--          .."\nPassword : "..password
+--          .."\nBSSID_set : "..bssid_set
+--          .."\nBSSID: "..bssid.."\n")
+--end
 
-function smart_connect()
-  wifi.startsmart(0, function() 
-                    print("Auto config success!")
-                    print_config()
-                    run()
-  end)
-end
+enduser_setup.start(
+  function()
+    print("Connected to wifi as:" .. wifi.sta.getip())
+    run()
+  end,
+  function(err, str)
+    print("enduser_setup: Err #" .. err .. ": " .. str)
+  end
+);
 
-if(ssid ~= '') then
-  print_config()
-  wifi.sta.autoconnect(1)
-  tmr.alarm(1, 5000, tmr.ALARM_SINGLE, function()
-              tmr.stop(0)
-              tmr.unregister(0)
-              smart_connect()
-  end)
-  tmr.alarm(0, 500, tmr.ALARM_AUTO, function() 
-              if wifi.sta.getip()==nil then 
-                print(" Wait for IP --> "..wifi.sta.status()) 
-              else 
-                print("New IP address is "..wifi.sta.getip()) 
-                tmr.stop(0) 
-                tmr.unregister(0)
-                print('load mqtt')
-                run()
-              end 
-  end)
-else    
-  smart_connect()
-end
+--function smart_connect()
+--  wifi.startsmart(0, function() 
+--                    print("Auto config success!")
+--                    print_config()
+--                    run()
+--  end)
+--end
+--
+--connected=true
+--if(ssid ~= '') then
+--  print_config()
+--  wifi.sta.autoconnect(1)
+--  tmr.alarm(1, 10000, tmr.ALARM_SINGLE, function()
+--    if(connected) then
+--              print("stop using previous config")
+--              tmr.stop(0)
+--              tmr.unregister(0)
+--              wifi.sta.config('','')
+--              smart_connect()
+--     end
+--  end)
+--  tmr.alarm(0, 500, tmr.ALARM_AUTO, function() 
+--              if wifi.sta.getip() == nil then 
+--                print(" Wait for IP --> "..wifi.sta.status()) 
+--              else
+--                connected=nil
+--                print("New IP address is "..wifi.sta.getip()) 
+--                tmr.stop(0) 
+--                tmr.unregister(0)
+--                print('load mqtt')
+--                run()
+--              end 
+--  end)
+--else    
+--  smart_connect()
+--end
